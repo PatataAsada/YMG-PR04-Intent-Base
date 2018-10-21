f6a8321baa4ec119d66a6f7b7eca21abaa5bc57c
Yeray <yeray3000@gmail.com>
2018-10-21 13:14:33 +0200

    ok, layouts have been set, now starting to work with intents (actually, i just started messing with phone and it works, so one less to do)

--- app/src/main/java/es/iessaladillo/pedrojoya/pr04/ui/avatar/AvatarActivity.java
+++ app/src/main/java/es/iessaladillo/pedrojoya/pr04/ui/avatar/AvatarActivity.java
@@ -1,19 +1,55 @@
 package es.iessaladillo.pedrojoya.pr04.ui.avatar;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import es.iessaladillo.pedrojoya.pr04.R;
 import es.iessaladillo.pedrojoya.pr04.utils.ResourcesUtils;
 
 public class AvatarActivity extends AppCompatActivity {
 
+    TextView lblAvatar1;
+    ImageView imgAvatar1;
+    TextView lblAvatar2;
+    ImageView imgAvatar2;
+    TextView lblAvatar3;
+    ImageView imgAvatar3;
+    TextView lblAvatar4;
+    ImageView imgAvatar4;
+    TextView lblAvatar5;
+    ImageView imgAvatar5;
+    TextView lblAvatar6;
+    ImageView imgAvatar6;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_avatar);
-        // TODO
+        initViews();
+        initListeners();
+    }
+
+    private void initListeners() {
+
+    }
+
+    private void initViews() {
+        lblAvatar1 = ActivityCompat.requireViewById(this, R.id.lblAvatar1);
+        imgAvatar1 = ActivityCompat.requireViewById(this, R.id.imgAvatar1);
+        lblAvatar2 = ActivityCompat.requireViewById(this, R.id.lblAvatar2);
+        imgAvatar2 = ActivityCompat.requireViewById(this, R.id.imgAvatar2);
+        lblAvatar3 = ActivityCompat.requireViewById(this, R.id.lblAvatar3);
+        imgAvatar3 = ActivityCompat.requireViewById(this, R.id.imgAvatar3);
+        lblAvatar4 = ActivityCompat.requireViewById(this, R.id.lblAvatar4);
+        imgAvatar4 = ActivityCompat.requireViewById(this, R.id.imgAvatar4);
+        lblAvatar5 = ActivityCompat.requireViewById(this, R.id.lblAvatar5);
+        imgAvatar5 = ActivityCompat.requireViewById(this, R.id.imgAvatar5);
+        lblAvatar6 = ActivityCompat.requireViewById(this, R.id.lblAvatar6);
+        imgAvatar6 = ActivityCompat.requireViewById(this, R.id.imgAvatar6);
     }
 
     // DO NO TOUCH
--- app/src/main/java/es/iessaladillo/pedrojoya/pr04/ui/main/MainActivity.java
+++ app/src/main/java/es/iessaladillo/pedrojoya/pr04/ui/main/MainActivity.java
@@ -1,20 +1,281 @@
 package es.iessaladillo.pedrojoya.pr04.ui.main;
 
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Objects;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import es.iessaladillo.pedrojoya.pr04.R;
+import es.iessaladillo.pedrojoya.pr04.data.local.Database;
+import es.iessaladillo.pedrojoya.pr04.data.local.model.Avatar;
+import es.iessaladillo.pedrojoya.pr04.utils.ValidationUtils;
 
 @SuppressWarnings("WeakerAccess")
 public class MainActivity extends AppCompatActivity {
+
+    private TextView lblAvatar;
+    private ImageView imgAvatar;
+    private TextView lblName;
+    private EditText txtName;
+    private TextView lblEmail;
+    private EditText txtEmail;
+    private ImageView imgEmail;
+    private TextView lblPhonenumber;
+    private EditText txtPhonenumber;
+    private ImageView imgPhonenumber;
+    private TextView lblAddress;
+    private EditText txtAddress;
+    private ImageView imgAddress;
+    private TextView lblWeb;
+    private EditText txtWeb;
+    private ImageView imgWeb;
+    private Intent intention;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // TODO
+        initViews();
+        initListeners();
+    }
+
+    private void initListeners() {
+        //Focus Listeners
+        focusListeners();
+
+        //Text change Listeners
+        textChangedListeners();
+
+        //Icon press listeners.
+        iconListeners();
+
+        //When pressing avatar image or name.
+        imgAvatar.setOnClickListener(v -> {
+
+        });
+        lblAvatar.setOnClickListener(v -> {
+
+        });
+    }
+
+    private void iconListeners() {
+        imgEmail.setOnClickListener(v -> {
+            intention = new Intent();
+            intention.setAction(Intent.ACTION_SEND);
+            intention.putExtra(Intent.EXTRA_EMAIL, txtEmail.getText());
+            startActivity(intention);
+        });
+        imgPhonenumber.setOnClickListener(v -> {
+            intention = new Intent(Intent.ACTION_DIAL);
+            intention.setData(Uri.parse("tel:"+txtPhonenumber.getText()));
+            startActivity(intention);
+        });
+    }
+
+    private void textChangedListeners() {
+        txtName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (count == 0) {
+                    txtName.setError(getString(R.string.main_invalid_data));
+                    lblName.setEnabled(false);
+                } else lblName.setEnabled(true);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        txtEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!ValidationUtils.isValidEmail(txtEmail.getText().toString())) {
+                    txtEmail.setError(getString(R.string.main_invalid_data));
+                    lblEmail.setEnabled(false);
+                    imgEmail.setEnabled(false);
+                } else {
+                    lblEmail.setEnabled(true);
+                    imgEmail.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        txtPhonenumber.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!ValidationUtils.isValidPhone(txtPhonenumber.getText().toString())) {
+                    txtPhonenumber.setError(getString(R.string.main_invalid_data));
+                    lblPhonenumber.setEnabled(false);
+                    imgPhonenumber.setEnabled(false);
+                } else {
+                    lblPhonenumber.setEnabled(true);
+                    imgPhonenumber.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        txtAddress.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (count == 0) {
+                    txtAddress.setError(getString(R.string.main_invalid_data));
+                    lblAddress.setEnabled(false);
+                    imgAddress.setEnabled(false);
+                } else {
+                    lblAddress.setEnabled(true);
+                    imgAddress.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        txtWeb.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!ValidationUtils.isValidUrl(txtWeb.getText().toString())) {
+                    txtWeb.setError(getString(R.string.main_invalid_data));
+                    lblWeb.setEnabled(false);
+                    imgWeb.setEnabled(false);
+                } else {
+                    lblWeb.setEnabled(true);
+                    imgWeb.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        //When pressing IME done button.
+        txtWeb.setOnEditorActionListener((v, actionId, event) -> {
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                save();
+                InputMethodManager imm =
+                        (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                Objects.requireNonNull(imm).hideSoftInputFromWindow(v.getWindowToken(), 0);
+                return true;
+            }
+            return false;
+        });
+    }
+
+    private void focusListeners() {
+        txtName.setOnFocusChangeListener((v, hasFocus) -> {
+            if (hasFocus) lblName.setTypeface(Typeface.DEFAULT_BOLD);
+            else lblName.setTypeface(Typeface.DEFAULT);
+        });
+        txtEmail.setOnFocusChangeListener((v, hasFocus) -> {
+            if (hasFocus) {
+                lblEmail.setTypeface(Typeface.DEFAULT_BOLD);
+            } else lblEmail.setTypeface(Typeface.DEFAULT);
+        });
+        txtPhonenumber.setOnFocusChangeListener((v, hasFocus) -> {
+            if (hasFocus) {
+                lblPhonenumber.setTypeface(Typeface.DEFAULT_BOLD);
+            } else lblPhonenumber.setTypeface(Typeface.DEFAULT);
+        });
+        txtAddress.setOnFocusChangeListener((v, hasFocus) -> {
+            if (hasFocus) {
+                lblAddress.setTypeface(Typeface.DEFAULT_BOLD);
+            } else lblAddress.setTypeface(Typeface.DEFAULT);
+        });
+        txtWeb.setOnFocusChangeListener((v, hasFocus) -> {
+            if (hasFocus) {
+                lblWeb.setTypeface(Typeface.DEFAULT_BOLD);
+            } else lblWeb.setTypeface(Typeface.DEFAULT);
+        });
+    }
+
+    private void initViews() {
+        //AVATAR
+        imgAvatar = ActivityCompat.requireViewById(this, R.id.imgAvatar);
+        lblAvatar = ActivityCompat.requireViewById(this, R.id.lblAvatar);
+        setDefault(imgAvatar, lblAvatar);
+        //NAME
+        lblName = ActivityCompat.requireViewById(this, R.id.lblName);
+        lblName.setTypeface(Typeface.DEFAULT_BOLD);
+        txtName = ActivityCompat.requireViewById(this, R.id.txtName);
+        //EMAIL
+        lblEmail = ActivityCompat.requireViewById(this, R.id.lblEmail);
+        txtEmail = ActivityCompat.requireViewById(this, R.id.txtEmail);
+        imgEmail = ActivityCompat.requireViewById(this, R.id.imgEmail);
+        imgEmail.setTag(R.drawable.ic_email_24dp);
+        //PHONENUMBER
+        lblPhonenumber = ActivityCompat.requireViewById(this, R.id.lblPhonenumber);
+        txtPhonenumber = ActivityCompat.requireViewById(this, R.id.txtPhonenumber);
+        imgPhonenumber = ActivityCompat.requireViewById(this, R.id.imgPhonenumber);
+        imgPhonenumber.setTag(R.drawable.ic_call_24dp);
+        //ADDRESS
+        lblAddress = ActivityCompat.requireViewById(this, R.id.lblAddress);
+        txtAddress = ActivityCompat.requireViewById(this, R.id.txtAddress);
+        imgAddress = ActivityCompat.requireViewById(this, R.id.imgAddress);
+        imgAddress.setTag(R.drawable.ic_map_24dp);
+        //WEB
+        lblWeb = ActivityCompat.requireViewById(this, R.id.lblWeb);
+        txtWeb = ActivityCompat.requireViewById(this, R.id.txtWeb);
+        imgWeb = ActivityCompat.requireViewById(this, R.id.imgWeb);
+        imgWeb.setTag(R.drawable.ic_web_24dp);
+    }
+
+    private void setDefault(ImageView imgAvatar, TextView nameAvatar) {
+        Avatar defAvatar = Database.getInstance().getDefaultAvatar();
+        imgAvatar.setImageResource(defAvatar.getImageResId());
+        imgAvatar.setTag(defAvatar.getImageResId());
+        nameAvatar.setText(defAvatar.getName());
     }
 
     // DO NOT TOUCH
@@ -35,7 +296,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void save() {
-        // TODO
+        boolean validation = true;
+        if (txtName.getText().toString().equals("")) {
+            validation = false;
+            txtName.setText("");
+        }
+        if (txtEmail.getText().toString().equals("")) {
+            validation = false;
+            txtEmail.setText("");
+        } else if (!ValidationUtils.isValidEmail(txtEmail.getText().toString())) {
+            validation = false;
+        }
+        if (txtPhonenumber.getText().toString().equals("")) {
+            validation = false;
+            txtPhonenumber.setText("");
+        } else if (!ValidationUtils.isValidPhone(txtPhonenumber.getText().toString()))
+            validation = false;
+
+        if (txtAddress.getText().toString().equals("")) {
+            validation = false;
+            txtAddress.setText("");
+        }
+        if (txtWeb.getText().toString().equals("")) {
+            validation = false;
+            txtWeb.setText("");
+        } else if (!ValidationUtils.isValidUrl(txtWeb.getText().toString())) validation = false;
+
+
+        if (validation) {
+            Snackbar.make(txtWeb, getText(R.string.main_saved_succesfully).toString(), Snackbar.LENGTH_SHORT).show();
+        } else
+            Snackbar.make(txtWeb, getText(R.string.main_error_saving).toString(), Snackbar.LENGTH_SHORT).show();
     }
 
 }
